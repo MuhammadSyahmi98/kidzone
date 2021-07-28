ff696763a4bca1f4d82c87b7989affe0665e9e1e
Muhammad Syahmi <syahmijalil12@gmail.com>
2021-07-28 13:09:13 +0800

    Push

--- resources/php/conn.php
+++ resources/php/conn.php
@@ -1,9 +1,9 @@
 <?php
 /* Database connection settings */
-	$servername = "localhost";
-    $username = "root";		//put your phpmyadmin username.(default is "root")
-    $password = "";			//if your phpmyadmin has a password put it here.(default is "root")
-    $dbname = "kidzone";
+$servername = "localhost";
+$username = "syahmi_user1";		//put your phpmyadmin username.(default is "root")
+$password = "XgZb8]?{Xn3s";			//if your phpmyadmin has a password put it here.(default is "root")
+$dbname = "syahmi_muhammad_kidzone";
     
 	$conn = mysqli_connect($servername, $username, $password, $dbname);
 	
